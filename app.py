888e702459c2a97ac8c6c4e79afca2c7d5df2cd5
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-02-19 14:04:10 -0300

    feat: Otimização do gerador de senhas

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
 import pandas as pd
 from PyPDF2 import PdfReader, PdfWriter
-import shutil
 
-# Excel
+# Manipulação do excel
 col_nomes = "NOME DOC"
 col_senhas = "SENHA"
 col_caminho = "DIRETORIO DOC"
-arquivo_excel = "./senha/Senhas_pdfs_assoc.xlsm"
-#arquivo_excel = "./senha/teste.xlsx"
+arquivo_excel = "./senha/Senhas_pdfs_assoc(gui).xlsm"
 
 read = pd.read_excel(arquivo_excel, dtype={col_senhas: str, col_nomes: str})
 
@@ -18,61 +16,66 @@ valor_caminho = read[col_caminho].tolist()
 
 dicionario = {id: str(senha) for id, senha in zip(valor_caminho, valor_senha)}
 
-# PDF
-index = 0
-count = 0
-erros = []
-mensagens = []
-writer = PdfWriter()
-
 # Compressor de PDF
 def comprimir_arquivo(arquivo):
-    global count
+    count = 1
     print(f"\nComprimindo arquivo, aguarde...\nNome: {valor_nome[count]}")
-    caminho_app = (r"C:\\Users\\Luccas\\Downloads\\gerador_senha\\pdfsizeopt\\pdfsizeopt")
+    caminho_app = (r"./pdfsizeopt/pdfsizeopt")
     arquivo_entrada = arquivo
     arquivo_saida = arquivo
     os.system(f"{caminho_app} --use-pngout=no {arquivo_entrada} {arquivo_saida}")
     print(f"\nCompressão concluída com sucesso!\nArquivo: {valor_nome[count]}")
 
-for pdf in valor_caminho:
-    tamanho_pdf = os.path.getsize(pdf)
-    pdf_kb = tamanho_pdf / 1024
-    if pdf_kb > 800:
-        comprimir_arquivo(pdf)
-        count += 1
-
 # Gerador de senha PDF
-for id, senha in dicionario.items():
+def gerador_senha(id, senha):
+    index = 0
+    erros = []
+    mensagens = []
+    count = 1
+
     try:
-        count = 1
-        reader = PdfReader(id)
-        if reader.is_encrypted:
-            print("Removendo senha...")
-            reader.decrypt(senha)
-        for page in reader.pages:
-            writer.add_page(page)
-            print(f"Lendo páginas, aguarde...\n{count}/{len(reader.pages)}")
-            count+=1
-            writer.encrypt(user_password=senha)
-            with open(id, "wb") as f:
-                writer.write(f)
-        print(f"Arquivo: {valor_nome[index]}\nSenha: {senha}\nPosição: {index+1}/{len(dicionario)}\n")
+        with open(id, "rb") as arquivo_entrada:
+            reader = PdfReader(arquivo_entrada)
+            writer = PdfWriter()
+            for page in reader.pages:
+                writer.add_page(page)
+                print("\nAdicionando senha, aguarde...")
+                if not reader.is_encrypted:
+                    print(f"Página: {count}/{len(reader.pages)}")
+                    writer.encrypt(user_password=senha)
+                    count+=1
+            with open(id, "wb") as arquivo_saida:
+                writer.write(arquivo_saida)
+            print(f"\nArquivo: {valor_nome[index]}\nSenha: {senha}\nPosição: {index+1}/{len(dicionario)}\n")
     except Exception as e:
         print(f"Erro no arquivo: {valor_nome[index]}\nErro: {e}\nPosição: {index+1}/{len(dicionario)}\n")
         erros.append(valor_nome[index])
         mensagens.append(e)
     index += 1
 
-print(f"Arquivos com sucesso: {len(dicionario) - len(erros)}\nArquivos com falhas: {len(erros)}")
+    print(f"Arquivos com sucesso: {len(dicionario) - len(erros)}\nArquivos com falhas: {len(erros)}")
 
+    # Log de erros
+    if len(erros) > 0:
+        index = 0
+        print(f"\nArquivos com falhas: {erros}\n")
+        print("=" * 30 + "\n" + "======== [LOG DE ERROS] ======\n" + "=" * 30 + "\n")
 
-# Log de erros
-if len(erros) > 0:
-    index = 0
-    print(f"\nArquivos com falhas: {erros}\n")
-    print("=" * 30 + "\n" + "======== [LOG DE ERROS] ======\n" + "=" * 30 + "\n")
+        for erro in erros:
+            print(f"Arquivo: {erro}\nMensagens: {mensagens[index]}\n")
+            index+1
 
-    for erro in erros:
-        print(f"Arquivo: {erro}\nMensagens: {mensagens[index]}\n")
-        index+1
+# Executar compressor de PDF
+'''
+for pdf in valor_caminho:
+    count = 0
+    tamanho_pdf = os.path.getsize(pdf)
+    pdf_kb = tamanho_pdf / 1024
+    if pdf_kb > 1000:
+        comprimir_arquivo(pdf)
+        count += 1
+'''
+
+# Executar gerador de senha para PDF
+for id, senha in dicionario.items():
+    gerador_senha(id, senha)
