38c114930ef1bf8b435565bd9784f0079fbc0c30
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-11-21 19:55:52 -0300

    [CODE REFACTORING]

--- teste.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-def comprimir_arquivo(arquivo):
-    os.system(f"C:\\Users\\Lucas.Aguiar\\Desktop\\gerador_senha\\pdfsizeopt\\pdfsizeopt.exe C:\\Users\\Lucas.Aguiar\\Desktop\\gerador_senha\\pdf\{arquivo} C:\\Users\\Lucas.Aguiar\\Desktop\\gerador_senha\\pdfsizeopt\\pdf\\{arquivo}")
-
-nome_arquivo = "teste.pdf"
-comprimir_arquivo(nome_arquivo)
