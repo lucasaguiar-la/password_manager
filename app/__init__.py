21850f9c4f8943c16076e17d73528dc1c97dd706
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-11-21 20:01:31 -0300

    [FEAT]: create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
